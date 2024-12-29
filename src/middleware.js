0c5a707587a8835154faf8c2f8e2167a5313f4de
RakhithJK <37833354+RakhithJK@users.noreply.github.com>
2024-12-29 12:52:00 +0530

    Add files via upload

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "805fc112-eed6-47dd-8f21-a8f4acaede41");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
